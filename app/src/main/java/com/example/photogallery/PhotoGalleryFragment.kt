887b0eedd5be1b0ceb87e90702e191867493d1c0
SoKratil <125564050+SoKratil@users.noreply.github.com>
2024-02-01 01:36:04 +0700

    upd2

--- app/src/main/java/com/example/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/com/example/photogallery/PhotoGalleryFragment.kt
@@ -1,26 +1,39 @@
 package com.example.photogallery
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bignerdranch.android.photogallery.api.FlickrApi
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
 
+private const val TAG = "PhotoGalleryFragment"
 class PhotoGalleryFragment : Fragment() {
     private lateinit var photoRecyclerView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val retrofit: Retrofit = Retrofit.Builder()
-            .baseUrl("https://www.flickr.com/")
-            .build()
+        val flickrLiveData: LiveData<String> = FlickrFetchr().fetchPhotos()
 
-        val flickrApi: FlickrApi = retrofit.create(FlickrApi::class.java)
+        flickrLiveData.observe(
+            this,
+            Observer { responseString ->
+                Log.d(TAG, "Response received: $responseString")
+            }
+        )
+
+
     }
 
 
--- app/src/main/java/com/example/photogallery/api/FlickrApi.kt
+++ app/src/main/java/com/example/photogallery/api/FlickrApi.kt
@@ -5,6 +5,13 @@ import retrofit2.http.GET
 
 interface FlickrApi {
 
-    @GET("/")
+    @GET("services/rest/? method=flickr.interestingness.getList" +
+        "&api_key=e232441422cf1f2616646a43b42d0325" +
+                "&format=json" +
+                "&nojsoncallback=1" +
+                "&extras=url_s")
     fun fetchPhotos(): Call<String>
 }
+
+
+
