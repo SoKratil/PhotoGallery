20d739ba6eda9b5d9848ea2825a1c3737a5a481b
SoKratil <125564050+SoKratil@users.noreply.github.com>
2024-02-06 00:42:42 +0700

    bill close up

--- app/src/main/java/com/example/photogallery/api/PhotoResponse.kt
+++ app/src/main/java/com/example/photogallery/api/PhotoResponse.kt
@@ -2,7 +2,7 @@ package com.example.photogallery.api
 
 import com.example.photogallery.GalleryItem
 import com.google.gson.annotations.SerializedName
-
+ 
 class PhotoResponse {
     @SerializedName("photo")
     lateinit var galleryItems: List<GalleryItem>
