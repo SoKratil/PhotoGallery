6eb86a7958fdc4489ed377fb04f394b3582de811
SoKratil <125564050+SoKratil@users.noreply.github.com>
2024-02-13 04:13:05 +0700

    Search work

--- app/src/main/java/com/example/photogallery/PhotoGalleryViewModel.kt
+++ app/src/main/java/com/example/photogallery/PhotoGalleryViewModel.kt
@@ -1,13 +1,26 @@
 package com.example.photogallery
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 
 class PhotoGalleryViewModel : ViewModel() {
+
     val galleryItemLiveData: LiveData<List<GalleryItem>>
 
-    init {
+    private val flickrFetchr = FlickrFetchr()
+    private val mutableSearchTerm = MutableLiveData<String>()
 
-        galleryItemLiveData = FlickrFetchr().searchPhotos("planets")
+
+    init {
+        mutableSearchTerm.value = "planets"
+        galleryItemLiveData = Transformations.switchMap(mutableSearchTerm) { searchTerm -> flickrFetchr.searchPhotos(searchTerm)
+        }
+
     }
+    fun fetchPhotos(query: String = "") {
+        mutableSearchTerm.value = query
+    }
+
 }
