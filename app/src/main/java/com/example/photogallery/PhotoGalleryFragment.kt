6ec97b44fa7bb393f2fd6c2689fec294c0c8c7c2
SoKratil <125564050+SoKratil@users.noreply.github.com>
2024-02-13 04:24:48 +0700

    7.3

--- app/src/main/java/com/example/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/com/example/photogallery/PhotoGalleryFragment.kt
@@ -104,6 +104,9 @@ class PhotoGalleryFragment : Fragment() {
                     return false
                 }
             })
+            setOnSearchClickListener { searchView.setQuery(photoGalleryViewModel.searchTerm, false)
+            }
+
         }
 
     }
